230316b5a77150103615368865677b673b3fa11f
Jeremy <32287708+jeremygblake@users.noreply.github.com>
2020-11-06 23:52:00 -0800

    initialize

---
+++ src/Components/shapes/square.js
@@ -0,0 +1,23 @@
+
+
+class square extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            color: black,
+            height: "10px",
+            width: "10px"
+        }
+    }
+        render()
+        {
+            return (
+                <div>
+                    
+                </div>
+            );
+        }
+}
